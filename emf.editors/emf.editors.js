47b5f75d5ae3b0183cdc3f22ed985c308914c849
CPAG Jenkins <mdevlin@epicor.com>
2016-11-17 23:26:16 -0800

    emf commit for v1.0.10-dev.195

--- emf.editors/emf.editors.js
+++ emf.editors/emf.editors.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.10-dev.194 built: 17-11-2016
+ * version:1.0.10-dev.195 built: 17-11-2016
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["editors"] = {"libName":"editors","version":"1.0.10-dev.194","built":"2016-11-17"};
+__ep_build_info["editors"] = {"libName":"editors","version":"1.0.10-dev.195","built":"2016-11-17"};
 
 (function() {
     'use strict';
