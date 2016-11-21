a979335dd425f62aefbd68e4a3bbcd5dfe1e914e
CPAG Jenkins <mdevlin@epicor.com>
2016-11-20 23:27:09 -0800

    emf commit for v1.0.10-dev.207

--- emf.tiles/emf.tiles.js
+++ emf.tiles/emf.tiles.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.10-dev.206 built: 20-11-2016
+ * version:1.0.10-dev.207 built: 20-11-2016
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["tiles"] = {"libName":"tiles","version":"1.0.10-dev.206","built":"2016-11-20"};
+__ep_build_info["tiles"] = {"libName":"tiles","version":"1.0.10-dev.207","built":"2016-11-20"};
 
 'use strict';
 /**
