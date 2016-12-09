f014b8640a322ab063be0ad0460786dd055cda19
CPAG Jenkins <mdevlin@epicor.com>
2016-12-08 17:27:10 -0800

    emf commit for v1.0.10-dev.286 build:534

--- emf.hybrid/emf.hybrid.js
+++ emf.hybrid/emf.hybrid.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.10-dev.285 built: 08-12-2016
+ * version:1.0.10-dev.286 built: 08-12-2016
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["hybrid"] = {"libName":"hybrid","version":"1.0.10-dev.285","built":"2016-12-08"};
+__ep_build_info["hybrid"] = {"libName":"hybrid","version":"1.0.10-dev.286","built":"2016-12-08"};
 
 (function() {
     'use strict';
