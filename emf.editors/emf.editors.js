4502a69d2d72a04b864ca5ebb84c9b9e46115c01
CPAG Jenkins <mdevlin@epicor.com>
2017-02-24 23:29:21 -0800

    emf commit for v1.0.10-dev.562 build:845

--- emf.editors/emf.editors.js
+++ emf.editors/emf.editors.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.10-dev.561 built: 24-02-2017
+ * version:1.0.10-dev.562 built: 24-02-2017
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["editors"] = {"libName":"editors","version":"1.0.10-dev.561","built":"2017-02-24"};
+__ep_build_info["editors"] = {"libName":"editors","version":"1.0.10-dev.562","built":"2017-02-24"};
 
 (function() {
     'use strict';
--- emf.embedded.apps/emf.embedded.apps.js
+++ emf.embedded.apps/emf.embedded.apps.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.10-dev.561 built: 24-02-2017
+ * version:1.0.10-dev.562 built: 24-02-2017
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["embedded.apps"] = {"libName":"embedded.apps","version":"1.0.10-dev.561","built":"2017-02-24"};
+__ep_build_info["embedded.apps"] = {"libName":"embedded.apps","version":"1.0.10-dev.562","built":"2017-02-24"};
 
 'use strict';
 /**
