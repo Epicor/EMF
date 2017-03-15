5d9a3e33f1283e4930442efaf26768c6b131648d
CPAG Jenkins <mdevlin@epicor.com>
2017-03-14 17:28:11 -0700

    emf commit for v1.0.12-dev.51 build:923

--- emf.chart/emf.chart.js
+++ emf.chart/emf.chart.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.12-dev.50 built: 14-03-2017
+ * version:1.0.12-dev.51 built: 14-03-2017
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["chart"] = {"libName":"chart","version":"1.0.12-dev.50","built":"2017-03-14"};
+__ep_build_info["chart"] = {"libName":"chart","version":"1.0.12-dev.51","built":"2017-03-14"};
 
 (function() {
     'use strict';
