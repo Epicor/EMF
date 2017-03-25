a65a025c6666e8b4a025507a9770ad12eb3eb356
CPAG Jenkins <mdevlin@epicor.com>
2017-03-24 23:30:07 -0700

    emf commit for v1.0.12-dev.95 build:967

--- emf.js
+++ emf.js
@@ -1,9 +1,9 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.12-dev.94 built: 24-03-2017
+ * version:1.0.12-dev.95 built: 24-03-2017
 */
 
-var __ep_build_info = { emf : {"libName":"emf","version":"1.0.12-dev.94","built":"2017-03-24"}};
+var __ep_build_info = { emf : {"libName":"emf","version":"1.0.12-dev.95","built":"2017-03-24"}};
 
 if (!epEmfGlobal) {
     var epEmfGlobal = {
--- emf.menu/emf.menu.js
+++ emf.menu/emf.menu.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.12-dev.94 built: 24-03-2017
+ * version:1.0.12-dev.95 built: 24-03-2017
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["menu"] = {"libName":"menu","version":"1.0.12-dev.94","built":"2017-03-24"};
+__ep_build_info["menu"] = {"libName":"menu","version":"1.0.12-dev.95","built":"2017-03-24"};
 
 (function() {
     'use strict';
