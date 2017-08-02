46b3618c68f80340c944275464368af4038d97b3
CPAG Jenkins <mdevlin@epicor.com>
2017-08-01 17:28:55 -0700

    emf commit for v1.0.14-dev.5 build:1385

--- emf.js
+++ emf.js
@@ -1,9 +1,9 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.14-dev.4 built: 01-08-2017
+ * version:1.0.14-dev.5 built: 01-08-2017
 */
 
-var __ep_build_info = { emf : {"libName":"emf","version":"1.0.14-dev.4","built":"2017-08-01"}};
+var __ep_build_info = { emf : {"libName":"emf","version":"1.0.14-dev.5","built":"2017-08-01"}};
 
 if (!epEmfGlobal) {
     var epEmfGlobal = {
@@ -16702,6 +16702,9 @@ angular.module('ep.embedded.apps').service('epEmbeddedAppsService', [
                 scope.filterState = {};
 
                 scope.initData = function() {
+
+                    scope.listSearch = '';
+                    scope.prevFirstLetter = '';
 
                     scope.searchType = 'text';
                     scope.searchPrompt = '';
--- emf.utilities/emf.utilities.js
+++ emf.utilities/emf.utilities.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.14-dev.4 built: 01-08-2017
+ * version:1.0.14-dev.5 built: 01-08-2017
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["utilities"] = {"libName":"utilities","version":"1.0.14-dev.4","built":"2017-08-01"};
+__ep_build_info["utilities"] = {"libName":"utilities","version":"1.0.14-dev.5","built":"2017-08-01"};
 
 (function() {
   'use strict';
