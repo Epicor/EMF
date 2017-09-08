5587909d2d5fa5b6afb3d23d9c9b50b9e9da700b
CPAG Jenkins <mdevlin@epicor.com>
2017-09-08 11:28:52 -0700

    emf commit for v1.0.14-dev.138 build:1520

--- emf.js
+++ emf.js
@@ -1,9 +1,9 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.14-dev.137 built: 08-09-2017
+ * version:1.0.14-dev.138 built: 08-09-2017
 */
 
-var __ep_build_info = { emf : {"libName":"emf","version":"1.0.14-dev.137","built":"2017-09-08"}};
+var __ep_build_info = { emf : {"libName":"emf","version":"1.0.14-dev.138","built":"2017-09-08"}};
 
 if (!epEmfGlobal) {
     var epEmfGlobal = {
