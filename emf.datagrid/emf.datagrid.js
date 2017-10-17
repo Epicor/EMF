43661392c826583cb2a7922748b5623a331df10a
CPAG Jenkins <mdevlin@epicor.com>
2017-10-16 17:33:08 -0700

    emf commit for v1.0.20-dev.21 build:1666

--- emf.datagrid/emf.datagrid.js
+++ emf.datagrid/emf.datagrid.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.20-dev.20 built: 16-10-2017
+ * version:1.0.20-dev.21 built: 16-10-2017
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["datagrid"] = {"libName":"datagrid","version":"1.0.20-dev.20","built":"2017-10-16"};
+__ep_build_info["datagrid"] = {"libName":"datagrid","version":"1.0.20-dev.21","built":"2017-10-16"};
 
 'use strict';
 /**
