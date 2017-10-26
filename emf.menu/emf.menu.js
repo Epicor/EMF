076c1ffd69ccfc1b5d24650e8980f78563adcba0
CPAG Jenkins <mdevlin@epicor.com>
2017-10-26 11:28:30 -0700

    emf commit for v1.0.20-dev.59 build:1706

--- emf.menu/emf.menu.js
+++ emf.menu/emf.menu.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.20-dev.58 built: 26-10-2017
+ * version:1.0.20-dev.59 built: 26-10-2017
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["menu"] = {"libName":"menu","version":"1.0.20-dev.58","built":"2017-10-26"};
+__ep_build_info["menu"] = {"libName":"menu","version":"1.0.20-dev.59","built":"2017-10-26"};
 
 (function() {
     'use strict';
