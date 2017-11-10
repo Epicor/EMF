b812ca75134a7226b2788638669d2fc3f72f624a
CPAG Jenkins <mdevlin@epicor.com>
2017-11-09 17:29:15 -0800

    emf commit for v1.0.24-dev.16 build:1765

--- emf.utilities/emf.utilities.js
+++ emf.utilities/emf.utilities.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.24-dev.15 built: 09-11-2017
+ * version:1.0.24-dev.16 built: 09-11-2017
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["utilities"] = {"libName":"utilities","version":"1.0.24-dev.15","built":"2017-11-09"};
+__ep_build_info["utilities"] = {"libName":"utilities","version":"1.0.24-dev.16","built":"2017-11-09"};
 
 (function() {
   'use strict';
