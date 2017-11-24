49248c7d1103f7f815e58c29d709dca1c9705ebf
CPAG Jenkins <mdevlin@epicor.com>
2017-11-24 05:28:45 -0800

    emf commit for v1.0.27-dev.38 build:1827

--- emf.customization/emf.customization.js
+++ emf.customization/emf.customization.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.27-dev.37 built: 23-11-2017
+ * version:1.0.27-dev.38 built: 24-11-2017
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["customization"] = {"libName":"customization","version":"1.0.27-dev.37","built":"2017-11-23"};
+__ep_build_info["customization"] = {"libName":"customization","version":"1.0.27-dev.38","built":"2017-11-24"};
 
 'use strict';
 /**
--- emf.js
+++ emf.js
@@ -1,9 +1,9 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.27-dev.37 built: 23-11-2017
+ * version:1.0.27-dev.38 built: 24-11-2017
 */
 
-var __ep_build_info = { emf : {"libName":"emf","version":"1.0.27-dev.37","built":"2017-11-23"}};
+var __ep_build_info = { emf : {"libName":"emf","version":"1.0.27-dev.38","built":"2017-11-24"}};
 
 if (!epEmfGlobal) {
     var epEmfGlobal = {
