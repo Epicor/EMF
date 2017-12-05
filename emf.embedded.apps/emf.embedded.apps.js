6259efb47a6516329d24bbb99b90cc95648d3073
CPAG Jenkins <mdevlin@epicor.com>
2017-12-04 17:31:39 -0800

    emf commit for v1.0.28-dev.25 build:1870

--- emf.embedded.apps/emf.embedded.apps.js
+++ emf.embedded.apps/emf.embedded.apps.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.28-dev.24 built: 04-12-2017
+ * version:1.0.28-dev.25 built: 04-12-2017
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["embedded.apps"] = {"libName":"embedded.apps","version":"1.0.28-dev.24","built":"2017-12-04"};
+__ep_build_info["embedded.apps"] = {"libName":"embedded.apps","version":"1.0.28-dev.25","built":"2017-12-04"};
 
 'use strict';
 /**
