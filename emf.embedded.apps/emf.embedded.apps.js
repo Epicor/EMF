b9d147ba763b6f0d6b1e37501b4cbe636beb9f8a
GitTask <gittask@notused.com>
2018-05-15 00:27:23 -0700

    emf commit for v1.0.32-dev.1 build:2145

--- emf.embedded.apps/emf.embedded.apps.js
+++ emf.embedded.apps/emf.embedded.apps.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.32 built: 14-05-2018
+ * version:1.0.32-dev.1 built: 15-05-2018
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["embedded.apps"] = {"libName":"embedded.apps","version":"1.0.32","built":"2018-05-14"};
+__ep_build_info["embedded.apps"] = {"libName":"embedded.apps","version":"1.0.32-dev.1","built":"2018-05-15"};
 
 'use strict';
 /**
--- emf.js
+++ emf.js
@@ -1,9 +1,9 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.32 built: 14-05-2018
+ * version:1.0.32-dev.1 built: 15-05-2018
 */
 
-var __ep_build_info = { emf : {"libName":"emf","version":"1.0.32","built":"2018-05-14"}};
+var __ep_build_info = { emf : {"libName":"emf","version":"1.0.32-dev.1","built":"2018-05-15"}};
 
 if (!epEmfGlobal) {
     var epEmfGlobal = {
