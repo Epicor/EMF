28014a1b1da81e0c302f65049cac5fbd282c7c10
GitTask <gittask@notused.com>
2018-05-16 00:28:15 -0700

    emf commit for v1.0.32-dev.2 build:2146

--- emf.utilities/emf.utilities.js
+++ emf.utilities/emf.utilities.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.0.32-dev.1 built: 15-05-2018
+ * version:1.0.32-dev.2 built: 16-05-2018
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["utilities"] = {"libName":"utilities","version":"1.0.32-dev.1","built":"2018-05-15"};
+__ep_build_info["utilities"] = {"libName":"utilities","version":"1.0.32-dev.2","built":"2018-05-16"};
 
 (function() {
   'use strict';
