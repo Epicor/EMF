37b683133d70c24410dfb2464122bb85de2a9b7d
GitTask <gittask@notused.com>
2018-08-10 00:27:47 -0700

    emf commit for v1.1.0-dev.63 build:2231

--- emf.embedded.apps/emf.embedded.apps.js
+++ emf.embedded.apps/emf.embedded.apps.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.62 built: 09-08-2018
+ * version:1.1.0-dev.63 built: 10-08-2018
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["embedded.apps"] = {"libName":"embedded.apps","version":"1.1.0-dev.62","built":"2018-08-09"};
+__ep_build_info["embedded.apps"] = {"libName":"embedded.apps","version":"1.1.0-dev.63","built":"2018-08-10"};
 
 'use strict';
 /**
