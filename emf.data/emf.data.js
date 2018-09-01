bfcda02323c0c979fe31c941092de07dbcf8499a
GitTask <gittask@notused.com>
2018-09-01 00:28:05 -0700

    emf commit for v1.1.0-dev.85 build:2255

--- emf.data/emf.data.js
+++ emf.data/emf.data.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.84 built: 31-08-2018
+ * version:1.1.0-dev.85 built: 01-09-2018
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["data"] = {"libName":"data","version":"1.1.0-dev.84","built":"2018-08-31"};
+__ep_build_info["data"] = {"libName":"data","version":"1.1.0-dev.85","built":"2018-09-01"};
 
 (function() {
     'use strict';
--- emf.js
+++ emf.js
@@ -1,9 +1,9 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.84 built: 31-08-2018
+ * version:1.1.0-dev.85 built: 01-09-2018
 */
 
-var __ep_build_info = { emf : {"libName":"emf","version":"1.1.0-dev.84","built":"2018-08-31"}};
+var __ep_build_info = { emf : {"libName":"emf","version":"1.1.0-dev.85","built":"2018-09-01"}};
 
 if (!epEmfGlobal) {
     var epEmfGlobal = {
