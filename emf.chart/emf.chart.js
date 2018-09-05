c112351b0a4f080d5bb3372f39d0ddc2c9e7d3ff
GitTask <gittask@notused.com>
2018-09-05 06:25:06 -0700

    emf commit for v1.1.0-dev.92 build:2262

--- emf.chart/emf.chart.js
+++ emf.chart/emf.chart.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.91 built: 05-09-2018
+ * version:1.1.0-dev.92 built: 05-09-2018
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["chart"] = {"libName":"chart","version":"1.1.0-dev.91","built":"2018-09-05"};
+__ep_build_info["chart"] = {"libName":"chart","version":"1.1.0-dev.92","built":"2018-09-05"};
 
 (function() {
     'use strict';
--- emf.js
+++ emf.js
@@ -1,9 +1,9 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.91 built: 05-09-2018
+ * version:1.1.0-dev.92 built: 05-09-2018
 */
 
-var __ep_build_info = { emf : {"libName":"emf","version":"1.1.0-dev.91","built":"2018-09-05"}};
+var __ep_build_info = { emf : {"libName":"emf","version":"1.1.0-dev.92","built":"2018-09-05"}};
 
 if (!epEmfGlobal) {
     var epEmfGlobal = {
