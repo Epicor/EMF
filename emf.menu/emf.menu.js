5311df3387a42d43f5a2fcaa513180535dcda175
GitTask <gittask@notused.com>
2018-09-10 00:27:35 -0700

    emf commit for v1.1.0-dev.98 build:2268

--- emf.menu/emf.menu.js
+++ emf.menu/emf.menu.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.97 built: 09-09-2018
+ * version:1.1.0-dev.98 built: 10-09-2018
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["menu"] = {"libName":"menu","version":"1.1.0-dev.97","built":"2018-09-09"};
+__ep_build_info["menu"] = {"libName":"menu","version":"1.1.0-dev.98","built":"2018-09-10"};
 
 (function() {
     'use strict';
