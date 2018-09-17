bbbbd319aa3d97369b75108a1e5ced0b12198021
GitTask <gittask@notused.com>
2018-09-17 00:27:54 -0700

    emf commit for v1.1.0-dev.105 build:2275

--- emf.shell/emf.shell.js
+++ emf.shell/emf.shell.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.104 built: 16-09-2018
+ * version:1.1.0-dev.105 built: 17-09-2018
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["shell"] = {"libName":"shell","version":"1.1.0-dev.104","built":"2018-09-16"};
+__ep_build_info["shell"] = {"libName":"shell","version":"1.1.0-dev.105","built":"2018-09-17"};
 
 if (!epEmfGlobal) {
     var epEmfGlobal = {
