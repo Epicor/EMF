bfeaeeffcbafe7fa2ccdd66392a9de56c95bb00b
GitTask <gittask@notused.com>
2018-09-18 00:28:39 -0700

    emf commit for v1.1.0-dev.106 build:2276

--- emf.js
+++ emf.js
@@ -1,9 +1,9 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.105 built: 17-09-2018
+ * version:1.1.0-dev.106 built: 18-09-2018
 */
 
-var __ep_build_info = { emf : {"libName":"emf","version":"1.1.0-dev.105","built":"2018-09-17"}};
+var __ep_build_info = { emf : {"libName":"emf","version":"1.1.0-dev.106","built":"2018-09-18"}};
 
 if (!epEmfGlobal) {
     var epEmfGlobal = {
