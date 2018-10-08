d42f93d15294e85acb8f54b94943b7610a3788dc
GitTask <gittask@notused.com>
2018-10-08 00:27:12 -0700

    emf commit for v1.1.0-dev.127 build:2297

--- emf.editors/emf.editors.js
+++ emf.editors/emf.editors.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.126 built: 07-10-2018
+ * version:1.1.0-dev.127 built: 08-10-2018
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["editors"] = {"libName":"editors","version":"1.1.0-dev.126","built":"2018-10-07"};
+__ep_build_info["editors"] = {"libName":"editors","version":"1.1.0-dev.127","built":"2018-10-08"};
 
 (function() {
     'use strict';
--- emf.js
+++ emf.js
@@ -1,9 +1,9 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.126 built: 07-10-2018
+ * version:1.1.0-dev.127 built: 08-10-2018
 */
 
-var __ep_build_info = { emf : {"libName":"emf","version":"1.1.0-dev.126","built":"2018-10-07"}};
+var __ep_build_info = { emf : {"libName":"emf","version":"1.1.0-dev.127","built":"2018-10-08"}};
 
 if (!epEmfGlobal) {
     var epEmfGlobal = {
