7c68f72670df487c466c821eb5267cc55e0221d9
GitTask <gittask@notused.com>
2018-10-14 00:27:21 -0700

    emf commit for v1.1.0-dev.133 build:2303

--- emf.chart/emf.chart.js
+++ emf.chart/emf.chart.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.132 built: 13-10-2018
+ * version:1.1.0-dev.133 built: 14-10-2018
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["chart"] = {"libName":"chart","version":"1.1.0-dev.132","built":"2018-10-13"};
+__ep_build_info["chart"] = {"libName":"chart","version":"1.1.0-dev.133","built":"2018-10-14"};
 
 (function() {
     'use strict';
