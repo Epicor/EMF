a3ab72d90c055f76032c9114da3960c730f706f5
GitTask <gittask@notused.com>
2019-02-04 00:27:26 -0800

    emf commit for v1.1.0-dev.162 build:2341

--- emf.customization/emf.customization.js
+++ emf.customization/emf.customization.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.161 built: 03-02-2019
+ * version:1.1.0-dev.162 built: 04-02-2019
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["customization"] = {"libName":"customization","version":"1.1.0-dev.161","built":"2019-02-03"};
+__ep_build_info["customization"] = {"libName":"customization","version":"1.1.0-dev.162","built":"2019-02-04"};
 
 'use strict';
 /**
