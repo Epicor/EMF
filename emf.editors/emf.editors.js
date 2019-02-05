96adbd502e380801fadd0ba7824369a66c048673
GitTask <gittask@notused.com>
2019-02-05 00:28:34 -0800

    emf commit for v1.1.0-dev.163 build:2342

--- emf.editors/emf.editors.js
+++ emf.editors/emf.editors.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.162 built: 04-02-2019
+ * version:1.1.0-dev.163 built: 05-02-2019
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["editors"] = {"libName":"editors","version":"1.1.0-dev.162","built":"2019-02-04"};
+__ep_build_info["editors"] = {"libName":"editors","version":"1.1.0-dev.163","built":"2019-02-05"};
 
 (function() {
     'use strict';
