344e91bcdef959e4a0312dfab6964b11148df7a8
GitTask <gittask@notused.com>
2019-02-09 00:28:49 -0800

    emf commit for v1.1.0-dev.167 build:2346

--- emf.tiles/emf.tiles.js
+++ emf.tiles/emf.tiles.js
@@ -1,10 +1,10 @@
 /*
  * emf (Epicor Mobile Framework) 
- * version:1.1.0-dev.166 built: 08-02-2019
+ * version:1.1.0-dev.167 built: 09-02-2019
 */
 
 if (typeof __ep_build_info === "undefined") {var __ep_build_info = {};}
-__ep_build_info["tiles"] = {"libName":"tiles","version":"1.1.0-dev.166","built":"2019-02-08"};
+__ep_build_info["tiles"] = {"libName":"tiles","version":"1.1.0-dev.167","built":"2019-02-09"};
 
 'use strict';
 /**
